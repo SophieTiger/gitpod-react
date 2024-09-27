8d4da30c1af734495e8563bf5016db6047a7c502
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-09-27 07:54:53 +0000

    Add StatefulGreetingWithCallback

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
 import StatefulGreeting from './components/StatefulGreeting';
+import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
 
 function App() {
   return (
     <div className="App">
       {/* <FunctionalGreetingWithProps greeting="Nice to meet you!" name="Mike" age="32" /> */}
-      <StatefulGreeting greeting="I'm a stateful greeting" name="Mike"/>
+      {/* <StatefulGreeting greeting="I'm a stateful greeting" name="Mike"/> */}
+      <StatefulGreetingWithCallback />
     </div>
   );
 }
