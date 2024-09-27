c091abf7c7ef32f8c68ed41228084622c96402a0
Sophie Tigerholm <sophie.tigerholm@gmail.com>
2024-09-27 08:13:22 +0000

    Add state to introduction and buttonText and change it once button is clicked

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
 import StatefulGreeting from './components/StatefulGreeting';
 import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
+import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
 
 function App() {
   return (
     <div className="App">
       {/* <FunctionalGreetingWithProps greeting="Nice to meet you!" name="Mike" age="32" /> */}
       {/* <StatefulGreeting greeting="I'm a stateful greeting" name="Mike"/> */}
-      <StatefulGreetingWithCallback />
+      {/* <StatefulGreetingWithCallback /> */}
+      <StatefulGreetingWithPrevState />
     </div>
   );
 }
